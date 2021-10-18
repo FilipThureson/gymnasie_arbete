a3984a3f10c29f484c2c7ffe930eb1e484c1d848
Filip Thuresson <filip.tthuresson@gmail.com>
2021-10-18 20:48:20 +0200

    Update head.blade.php

--- app/resources/views/components/head.blade.php
+++ app/resources/views/components/head.blade.php
@@ -6,5 +6,5 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-<link rel="icon" href="bilder/AUXILIUM_LOGO.png" type="image/icon type">
+<link rel="icon" href="{{ asset('img/AUXILIUM_LOGO.png') }}" type="image/icon type">
 <script src="{{ asset('js/javascript.js') }}" defer></script>
