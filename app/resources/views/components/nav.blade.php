1204258e7e49128ca233775ec0cd8f8132b94030
angel-str <60510865+angel-str@users.noreply.github.com>
2021-10-18 15:52:23 +0200

    static html
    wefawf

--- app/resources/views/components/nav.blade.php
+++ app/resources/views/components/nav.blade.php
@@ -1,4 +1,14 @@
-<div style="display: flex; align-items: center">
-    <a href="/logout">Logout!</a>
+<!--<div style="display: flex; align-items: center">
+    
     <img style="border-radius: 50%;"src="{{Session::get('avatar')}}"></img>
-</div>
+</div>-->
+<header>
+    <a href="#"><img src="logo.png" alt="AUXILIUM LOGO"></a>
+    <ul>
+      <li><a href="#">Dina frågor</a></li>
+      <li><a href="#">Schema</a></li>
+      <li><a href="#">Om oss</a></li>
+      <a href="/logout">Logout!</a>
+      <a href="#"><img src="{{Session::get('avatar')}}" alt="Profile img" id="icon"></a>
+    </ul>
+  </header>
