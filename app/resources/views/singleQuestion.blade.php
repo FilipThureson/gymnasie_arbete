c71cd2cb51db7cb4a6d79d3bf1d6249e057129ee
Filip Thuresson <filip.tthuresson@gmail.com>
2021-10-22 17:43:10 +0200

    Update singleQuestion.blade.php

--- app/resources/views/singleQuestion.blade.php
+++ app/resources/views/singleQuestion.blade.php
@@ -2,12 +2,25 @@
 <html lang="en">
 <head>
     @include('components/head')
+    <style>
+        main{
+            width: 100%;
+            height: calc(100% - 100px);
+        }
+        main h1{
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%)
+        }
+    </style>
 </head>
 <body>
     @include('components/nav')
-    <div class="single_question_wrapper">
-        
-    </div>
+    <main>
+        <h1>Under Construction</h1>
+    </main>
     @include('components/footer')
     <script src="{{ asset('js/singleQuestion.js') }}"></script>
 </body>
