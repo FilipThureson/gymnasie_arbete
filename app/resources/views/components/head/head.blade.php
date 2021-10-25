693d2aa1f9943f11cef6d23103c95584c726a2b2
Filip Thuresson <60510154+FilipThureson@users.noreply.github.com>
2021-10-25 08:54:56 +0200

    Update head.blade.php

--- app/resources/views/components/head/head.blade.php
+++ app/resources/views/components/head/head.blade.php
@@ -9,3 +9,8 @@
 <link rel="icon" href="{{ asset('img/AUXILIUM_LOGO.png') }}" type="image/icon type">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=MML_HTMLorMML"></script>
+<script>
+    window.MathJax = { MathML: { extensions: ["mml3.js", "content-mathml.js"]}};
+</script>
