5a9796f715cbc00dea77e1d4e5e698310ef4449e
Filip Thuresson <60510154+FilipThureson@users.noreply.github.com>
2021-10-26 11:43:15 +0200

    Update head.blade.php

--- app/resources/views/components/head/head.blade.php
+++ app/resources/views/components/head/head.blade.php
@@ -13,3 +13,8 @@
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<script>
+    MathJax.Hub.Config({
+        "HTML-CSS": {noReflows: false}
+    });
+</script>
