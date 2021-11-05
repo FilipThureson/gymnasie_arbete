48054d4c0174ed0ca13bbc934ad2e2bc08db6ef3
Filip Thuresson <filip.tthuresson@gmail.com>
2021-11-05 12:37:15 +0100

    Update socket_server.js

--- app/socket_server.js
+++ app/socket_server.js
@@ -2,27 +2,42 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+
 const server = http.createServer(app);
+
+var https = require("https"),
+    fs = require("fs"),
+    server = https.createServer(
+        {
+            key: fs.readFileSync("/ssl.key"),
+            cert: fs.readFileSync("/te-auxilium.se.crt"),
+            ca: [fs.readFileSync("/te-auxilium.se.ca-bundle")]
+        }, app);
 
 const io = require("socket.io")(server, {
     cors: {
         origin: '*',
     }
 });
-
 app.use(cors());
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
 
-    socket.on('room', (data)=>{
+io.on('connection', (socket) => {
+    console.log('a user connected id:' + socket.id);
+
+    socket.on('room', (data) => {
         socket.join(data);
     });
-    socket.on('update',(data)=>{
+
+    socket.on('update_from_server', (data) => {
+        console.log(data);
         io.to(data).emit('update', socket.id);
     });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(8080, () => {
+    console.log('listening on *:8080');
 });
