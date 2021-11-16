047c9afb5fd44ce54f34a9caccb697cd697f5711
Filip Thuresson <filip.tthuresson@gmail.com>
2021-11-16 21:55:58 +0100

    w
    jekgt

--- app/resources/views/components/nav.blade.php
+++ app/resources/views/components/nav.blade.php
@@ -1,9 +1,16 @@
 <header>
-  <a href="/"><img src="{{ asset('img/AUXILIUM_SIDE.png') }}" alt="AUXILIUM LOGO"></a>
-  <ul>
+  <a id="logotype" href="/"><img src="{{ asset('img/AUXILIUM_SIDE.png') }}" alt="AUXILIUM LOGO"></a>
+  <div class="hamburger">
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+  </div>
+  <ul id="nav-links">
     <li><a href="/questions/user/{{ Session::get('email') }}">Dina frågor</a></li>
     <li><a target="_blank" href="https://web.skola24.se/timetable/timetable-viewer/kunskapsforbundetvast.skola24.se/Birger%20Sj%C3%B6bergsgymnasiet/">Schema</a></li>
     <li><a href="/logout">Logga Ut</a></li>
-    <a href="#"><img src="{{Session::get('avatar')}}" alt="Profile img" id="icon"></a>
+    <a href="#"><img src="{{Session::get('avatar')}}" alt="Profile img" id="icon" class="icon"></a>
   </ul>
 </header>
+
+<script src="{{ asset('js/menu.js') }}"></script>
