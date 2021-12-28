a42f1ca86672bf148fb98e7d4d8f6e78b80e5670
adasva1 <60510204+adasva1@users.noreply.github.com>
2021-12-28 21:12:11 +0100

    quoteRandom

--- static_html/menu.js
+++ static_html/menu.js
@@ -1,14 +1,13 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector("#nav-links");
-const links = document.querySelectorAll("#nav-links li");
-const icon = document.querySelector("#icon");
+var quotes = [
+  '“Failure is simply the opportunity to begin again, this time more intelligently.” -Henry Ford',
+  '“A person who never made a mistake never tried anything new.” -Albert Einstein',
+  '“Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.” -Martin Luther King, Jr.',
+  '“Kindness is the language which the deaf can hear and the blind can see.” -Mark Twain',
+  '“Our greatest fear should not be of failure but of succeeding at things in life that don’t really matter.” -Francis Chan'
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
- 
-    icon.classList.toggle("icon");
-  
-});
+]
+
+function newQuote() {
+  var randomQuote = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomQuote];
+}
